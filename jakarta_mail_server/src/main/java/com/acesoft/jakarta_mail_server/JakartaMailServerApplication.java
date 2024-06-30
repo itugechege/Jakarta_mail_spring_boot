a9d7cdf805b438bb6fa93f4a1c9a182890155696
itugealex <itugechege@gmail.com>
2024-06-30 13:18:03 +0300

    initial setup

---
+++ jakarta_mail_server/src/main/java/com/acesoft/jakarta_mail_server/JakartaMailServerApplication.java
@@ -0,0 +1,13 @@
+package com.acesoft.jakarta_mail_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakartaMailServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JakartaMailServerApplication.class, args);
+    }
+
+}
---
+++ jakarta_mail_server/src/test/java/com/acesoft/jakarta_mail_server/JakartaMailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acesoft.jakarta_mail_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakartaMailServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
